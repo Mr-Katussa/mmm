eba93f790cf02c6d558d7ffcb1e88081a5990a0e
MR Katussa <108258060+Mr-Katussa@users.noreply.github.com>
2023-03-26 21:18:58 -0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,13 +4,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*Contact Owner:\n*wa.me/923470027813?text=Assalam+o+alaikum*`
+let str = `Contact Owner:\n*wa.me/94764497078*`
     let waifu = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/waifu.txt')
     let chen = await waifu.text()
     let ran = chen.split('\n')
     let yae = ran[Math.floor(Math.random() * ran.length)]
     let thumb = await(await fetch(yae)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm,'OKAY','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: global.sig,
     title: wm3,
     body: 'want source code?',
